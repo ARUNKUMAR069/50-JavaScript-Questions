528e289c2d2aca05297a1563c4e217b121de591c
Arun Kumar <stockerunknown@gmail.com>
2025-02-09 22:29:48 +0530

    Problem 4 Sucessfully solved : Create two variables for first and last name concatenate and log them

---
+++ Question4/Question4.js
@@ -0,0 +1,7 @@
+//  Create two variables for first and last name concatenate and log them 
+firstName="Arun"
+lastName="Kumar"
+fullName= firstName+" "+lastName
+console.log(fullName)
+//  or 
+console.log(`${firstName} ${lastName}`)
