c9f8ba7a5a4941114e84130fcbf02fde677d82e6
Arun Kumar <stockerunknown@gmail.com>
2025-02-12 14:52:37 +0530

    Sucessfuly Solved Question Number 31

---
+++ Question31/Question31.js
@@ -0,0 +1,8 @@
+//  Create an array of your top 5 favourite movies and log it
+
+arr=["Peaky Blenders","Stranger Things","Beauty of the Beast","Bahubali","Indiana Jones"]
+console.log(arr)
+
+arr.forEach(function(value){
+console.log(value)
+})
