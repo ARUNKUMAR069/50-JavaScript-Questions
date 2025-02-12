9bf15a3f1795e311e7dfcefa23dc4259ddc73a68
Arun Kumar <stockerunknown@gmail.com>
2025-02-12 14:22:35 +0530

    New way to solve question 28

--- Question28/Question28.js
+++ Question28/Question28.js
@@ -1,8 +1,21 @@
 //  Use for loop to reverse an array [1,2,3,4]
 
-let actualarr = [1, 2, 3, 4,5,6,7,8,9,10]
+let actualarr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 let revarr = []
-for (let i =actualarr.length-1; i >= 0; i--) {
+for (let i = actualarr.length - 1; i >= 0; i--) {
     revarr.push(actualarr[i])
 }
 console.log(revarr)
+
+
+// or 
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+for (let i = 0; i < Math.floor(arr.length / 2); i++) {
+
+    let temp = arr[i]
+    arr[i] = arr[arr.length - 1 - i]
+    arr[arr.length - 1 - i] = temp
+}
+console.log(arr)
